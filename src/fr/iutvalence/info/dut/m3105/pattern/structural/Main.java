106ca18a40943ea90156a54ba20629bfe8f39d4d
Axel CÉARD <axel.ceard@iut-valence.fr>
2015-12-01 16:58:51 +0100

    END

--- src/fr/iutvalence/info/dut/m3105/pattern/structural/Main.java
+++ src/fr/iutvalence/info/dut/m3105/pattern/structural/Main.java
@@ -8,8 +8,14 @@ public class Main
 		Stack<String> stack = new ArrayStack<String>();
 		new StackOfStringTester(stack).testStack();
 		
+		System.out.println("\n\n");
 		List<String> list = new ArrayList<String>();
 		new StackOfStringTester(new ArrayListAdaptor(list)).testStack();
+		
+		System.out.println("\n\n");
+		List<String> list2 = new ArrayList<String>();
+		new StackOfStringTester(new StackProxyNoPeek(new ArrayListAdaptor(list2))).testStack();
+		
 	}
 
 }
